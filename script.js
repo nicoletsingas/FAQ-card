11ee9071135ae3bcdeaf3c9a0f71a436af00dcc5
Nicole Tsingas <nicolecass27@gmail.com>
2023-02-09 10:20:03 -0300

    Projeto FAQ Card - Challenge para Front-end Mentor

---
+++ script.js
@@ -0,0 +1,12 @@
+const questions = document.querySelectorAll('.question-icon');
+
+questions.forEach((question) => {
+    question.addEventListener('click', () => {
+        if(question.parentNode.classList.contains('active')){
+            question.parentNode.classList.toggle('active');
+        } else{
+            questions.forEach((question) => question.parentNode.classList.remove('active'));
+            question.parentNode.classList.add('active');
+        }
+    })
+});
